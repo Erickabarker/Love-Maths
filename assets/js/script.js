011556acde27b7a350e4664e3c2d0673e59af0eb
Erickabarker <110427318+Erickabarker@users.noreply.github.com>
2022-11-21 21:08:12 +0000

    set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Connected!")
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestions(){
+
+}
+
+function displaySubtractQuestions(){
+
+}
+
+function displayMultiplyQuestions(){
+
+}
